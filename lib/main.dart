fc616b95a609f88022c1803d3c9cb8c615da1d48
YacubeK <rahimzhan10@gmail.com>
2020-11-12 17:13:11 +0600

    feat: главный функционал игры

--- lib/main.dart
+++ lib/main.dart
@@ -5,113 +5,188 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+       
+        primarySwatch: Colors.lightGreen,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List<List> _matrix;
+
+  _HomePageState() {
+     _initMatrix();
+  }
+
+  _initMatrix() {
+   _matrix = List<List>(3);
+    for (var i = 0; i < _matrix.length; i++) {
+      _matrix[i] = List(3);
+      for (var j = 0; j < _matrix[i].length; j++) {
+        _matrix[i][j] = ' ';
+      }
+  }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("TIC TAC TOE"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+        child:Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                _buildElement(0,0),
+                _buildElement(0,1),
+                _buildElement(0,2),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                _buildElement(1,0),
+                _buildElement(1,1),
+                _buildElement(1,2),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                _buildElement(2,0),
+                _buildElement(2,1),
+                _buildElement(2,2),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  String _lastChar = 'o';
+
+  _buildElement(int i, int j) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    return GestureDetector(
+      onTap: () {
+        _setMatrixField(i,j);
+
+        if(_checkWinner(i,j)) {
+          _showDialog(_matrix[i][j]);
+        } else if(_checkDraw()){
+          _showDialog(null);
+        }
+      },
+          child: Container(
+        width: screenWidth * 0.32,
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          border: Border.all(
+            color: Colors.black
+          )
+        ),
+        child: Center(
+          child: Text(
+            _matrix[i][j],
+            style: TextStyle(color: Colors.black, fontSize: 92),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _setMatrixField(int i, int j) {
+    setState(() {
+          if (_matrix[i][j] == ' ') {
+        if (_lastChar == 'X')
+          _matrix[i][j] = 'O';
+
+        else
+          _matrix[i][j] = 'X';
+
+        _lastChar = _matrix[i][j];
+      }
+        });
+  }
+
+    _checkDraw() {
+    var draw = true;
+    _matrix.forEach((i) {
+      i.forEach((j) {
+        if (j == ' ')
+          draw = false;
+      });
+    });
+    return draw;
+  }
+
+  _checkWinner(int x, int y) {
+    var col = 0, row = 0, diag = 0, rdiag = 0;
+    var n = _matrix.length-1;
+    var player = _matrix[x][y];
+
+    for (int i = 0; i < _matrix.length; i++) {
+      if (_matrix[x][i] == player)
+        col++;
+      if (_matrix[i][y] == player)
+        row++;
+      if (_matrix[i][i] == player)
+        diag++;
+      if (_matrix[i][n-i] == player)
+        rdiag++;
+    }
+    if (row == n+1 || col == n+1 || diag == n+1 || rdiag == n+1) {
+      return true;
+    }
+    return false;
+  }
+
+  _showDialog(String winner) {
+    String dialogText;
+    if(winner == null) {
+      dialogText = 'It\'s a draw';
+    } else {
+      dialogText = 'Player $winner won';
+    }
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Game over'),
+          content: Text(dialogText),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Reset Game'),
+              onPressed: () {
+                 Navigator.of(context).pop();
+                setState(() {
+                  _initMatrix();
+                });
+              },
+            )
+          ],
+        );
+      } 
+      );
+  }
+
 }
